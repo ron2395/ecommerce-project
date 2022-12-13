6122aab0a2c613235b793e8b8fb2b5a0639cedd0
Rohan Sharma <ron.sharma2395@gmail.com>
2022-12-13 21:32:56 +0530

    done

--- backend/server.js
+++ backend/server.js
@@ -26,6 +26,7 @@ app.use(function (req, res, next) {
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
   next();
 });
 
